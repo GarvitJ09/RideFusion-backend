e29884a0e466002f986f5b7de16e61bbc769f489
GarvitJ09 <garvit992001@gmail.com>
2024-08-25 22:58:13 +0530

    updated code

--- routes/olaController.js
+++ routes/olaController.js
@@ -3,8 +3,14 @@ const router = express.Router();
 
 const { startRideSearch } = require("../service/olaRideSearch.js");
 
+const { cancelOlaRide } = require("../service/cancelOlaRide.js");
+
 router.post("/ride-search", (req, res, next) => {
   startRideSearch(req, res, next);
+});
+
+router.post("/ride-cancel", (req, res, next) => {
+  cancelOlaRide(req, res, next);
 });
 
 module.exports = router;
